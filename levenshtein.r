fc06970935afd3e5d07c7c9d2d3702fce603e3f3
vogelweide1985 <vogelweide1985@hotmail.de>
2021-03-18 13:54:50 +0100

    added recodes and conufsion matrices

--- levenshtein.r
+++ levenshtein.r
@@ -1,26 +1,27 @@
 require(dplyr)
 require(tidyr)
 require(purrr)
-
+require(forcats)
+require(caret) # only for confusion matrix
 # Set Language to German
 Sys.setenv(LANG = "de_DE.UTF-8")
 
 
 
 #Creating Dictionary with aliases with banks of interest
-dict <- list("Deutsche Bank" = c("deutsche bank", "db"),
+dict <- list("Deutsche_Bank" = c("deutsche bank", "db"),
              "Commerzbank" = c("commerzbank"),
              "Postbank" = c("postbank"), 
              "Hypovereinsbank" = c("hypo vereinsbank", "hypo", "hypobank"),
              "Targobank" = c("targobank", "targo"),
              "Sparkasse" = c("sparkasse", "kreissparkasse", "stadtsparkasse"),
-             "Volks- und Raiffeisenbank" = c( "v+r", "volksbank", "raiffeisenbank", "volks und raiffeisenbank"),
+             "Volks_und_Raiffeisenbank" = c( "v+r", "volksbank", "raiffeisenbank", "volks und raiffeisenbank"),
              "Ingdiba" = c("ing diba", "ing", "ingdiba", "diba"),
              "Santander"= c("santander"),
              "Spardabank" = c("sparda-bank", "sparda"),
-             "Union Invest" = c("union invest", "union"),
+             "Union_Invest" = c("union invest", "union"),
              "N26" = c("n26"),
-             "No answer" = c("keine", "keine ahnung"))
+             "No_answer" = c("keine", "keine ahnung"))
 
 
 
@@ -108,7 +109,7 @@ df_unique$lvl_2_1 <- df_unique$ldm
 
 # Setting specific distance cuts, so classification is stopped, when dist is to far
 df_unique$lvl_2_2<- ifelse(df_unique$min_distance<= 2, df_unique$ldm, NA)
-df_unique$lvl_2_3<- ifelse(   (df_unique$min_distance <= 2 & nchar(df_unique$answer) >=2) | 
+df_unique$lvl_2_3<- ifelse(   (df_unique$min_distance <= 2 & nchar(df_unique$answer) >3) | 
                               (df_unique$min_distance <= 4 & nchar(df_unique$answer) >=8) | 
                               (df_unique$min_distance <= 6 & nchar(df_unique$answer) >=12), df_unique$ldm, NA)
 
@@ -116,4 +117,69 @@ df_unique$lvl_2_3<- ifelse(   (df_unique$min_distance <= 2 & nchar(df_unique$ans
 # Level 3: Bringing levenshtein and Matching together
 
 df_unique$lvl_3 <- ifelse(!is.na(df_unique$lvl_1), df_unique$lvl_1, df_unique$lvl_2_3)
+
+
+# Level 4: Manual Recoding to calculate confusion Matrix | not perfect!!!
+
+correct_na <- c("dkb", "dbk", "spar", "debk", "dibs", "dinga", "inba", "tarox", "voksdbank", "bausparkasse",
+                "audibank", "apo bank", "digabank", "ergo bank", "volkswagenbank", "deutsche kreditbank",
+                "volkswagen bank", "bausparkasse sha", "spreewaldbank")
+
+correct_commerz <- c("commerz", "coba", "comerz", "commerbacg", "comerzbach", "comerz finanz	")
+correct_raiffeisenbank <- c("voba", "raiba", "v+r", "reiba", "volks", "r v", "r*v", "r/v", 	"raifeisen", "v+r bank", "raiffeinsen",
+  "reiffeisen", "raifeissen", "raiifeisen	", "vb bank", "vr bamk", "vr bsnk", "r&v bank", "v&r bank",
+  "volks- uns raiffeisen", "volks-undraiffeisen", "volks u. raiffeisen", "volks+raiffeisen bank",
+  "volks- reifeisenbank", "	volks reifen bank", "r u v bank", "r v banken", "r&v banken", "v + r banken", 
+  "v r banken", "voba/raiffeisen", "volks-raiffeinsenbanken", "volks-reifeisenbank", "volks und raiba",
+  "volksreifeisen bank", "volks-raiffeisen", "r+v meine bank", "vb heilbronn", "volks-raiffeissen", 
+  "vr bank flaeming")
+correct_post <- c("post", "deutsche postbank")
+correct_n26 <- c("no. 26", "bank 26", "n25 bank")
+correct_sparda <- c("spadaka")
+correct_ingdiba <- c("inkbiba")
+correct_sparkasse <- c("haspa", "nospa", "kreissparkassse koeln", "landesspaarkasse", "stadtsparkass muenchen", "sparkasse boeblingen",
+  "spk mittelthueringen	")
+correct_db <- c("dt bank", "d. bank", "dt.bank")
+
+#Recoding lvl_3 with manual recodes to lvl_4, the "reference"
+df_unique$lvl_4 <-  df_unique$lvl_3 
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_na, NA, df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_commerz, "commerzbank", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_raiffeisenbank, "raiffeisenbank", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_post, "postbank", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_n26, "n26", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_sparda, "sparda", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_ingdiba, "ingdiba", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_sparkasse , "sparkasse", df_unique$lvl_4)
+df_unique$lvl_4 <- ifelse(df_unique$answer %in% correct_db  , "db", df_unique$lvl_4)
+
+
+#Recoding all Alisases to names and building factors for confusion matrix
+#Could be purred -> lazy
+df_unique$lvl_0_rec <- as_factor(df_unique$lvl_0) %>% fct_collapse(!!!dict, ) %>% fct_explicit_na()
+df_unique$lvl_1_rec <- as_factor(df_unique$lvl_1) %>% fct_collapse(!!!dict)  %>% fct_explicit_na()
+df_unique$lvl_2_1_rec <- as_factor(df_unique$lvl_2_1) %>% fct_collapse(!!!dict) %>% fct_explicit_na()
+df_unique$lvl_2_2_rec <- as_factor(df_unique$lvl_2_2) %>% fct_collapse(!!!dict) %>% fct_explicit_na()
+df_unique$lvl_2_3_rec <- as_factor(df_unique$lvl_2_3) %>% fct_collapse(!!!dict) %>% fct_explicit_na()
+df_unique$lvl_3_rec <- as_factor(df_unique$lvl_3) %>% fct_collapse(!!!dict) %>% fct_explicit_na()
+df_unique$lvl_4_rec <- as_factor(df_unique$lvl_4) %>% fct_collapse(!!!dict) %>% fct_explicit_na()
+
+
+#Final Results -> Based on Unique ansers
+caret::confusionMatrix(df_unique$lvl_0_rec, df_unique$lvl_4_rec )
+caret::confusionMatrix(df_unique$lvl_1_rec, df_unique$lvl_4_rec )
+caret::confusionMatrix(df_unique$lvl_2_1_rec, df_unique$lvl_4_rec )
+caret::confusionMatrix(df_unique$lvl_2_2_rec, df_unique$lvl_4_rec )
+caret::confusionMatrix(df_unique$lvl_2_3_rec, df_unique$lvl_4_rec )
+caret::confusionMatrix(df_unique$lvl_3_rec, df_unique$lvl_4_rec )
+
+#Final Results -> Based on answers x Frequency
+#Again lazy copy-paste coding, sorry
+df_unique_expanded <- df_unique[rep(row.names(df_unique), df_unique$n),] # Expand df
+caret::confusionMatrix(df_unique_expanded$lvl_0_rec, df_unique_expanded$lvl_4_rec )
+caret::confusionMatrix(df_unique_expanded$lvl_1_rec, df_unique_expanded$lvl_4_rec )
+caret::confusionMatrix(df_unique_expanded$lvl_2_1_rec, df_unique_expanded$lvl_4_rec )
+caret::confusionMatrix(df_unique_expanded$lvl_2_2_rec, df_unique_expanded$lvl_4_rec )
+caret::confusionMatrix(df_unique_expanded$lvl_2_3_rec, df_unique_expanded$lvl_4_rec )
+caret::confusionMatrix(df_unique_expanded$lvl_3_rec, df_unique_expanded$lvl_4_rec )
 
